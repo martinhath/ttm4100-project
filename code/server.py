1582e8330da3fd91052a1cde553be1a35e516018
Martin Thoresen <martinhath@gmail.com>
2015-02-25 19:41:55 +0100

    Enforcer python 3+

--- code/server.py
+++ code/server.py
@@ -1,17 +1,19 @@
-from sys import argv
+# -*- coding: utf-8 -*-
+from sys import argv, version_info
 
 
 def print_usage():
     print('Usage: python server.py <port>')
 
-
 def server():
-    if len(argv) < 2:
-        print_usage()
-        return
     port = argv[1]
     print('Port: ', port)
 
 # Hvis vi kjører filen med `python server.py`
 if __name__ == '__main__':
-    server()
+    if len(argv) < 2:
+        print_usage()
+    elif version_info < (3, 0): 
+        print('Python 3.0 or higher required')
+    else:
+        server()
