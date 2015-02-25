f7e61f759cf452413b5d580f83e7ebcf20ba60b8
Martin Thoresen <martinhath@gmail.com>
2015-02-25 20:51:32 +0100

    Fungerer bedre

---
+++ code/models.py
@@ -0,0 +1,11 @@
+class User:
+
+    def __init__(self, username, password):
+        pass
+
+
+class Message:
+
+    def __init__(self, user, message):
+        pass
+
--- code/server.py
+++ code/server.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 from sys import argv, version_info
 import socketserver
+from json import loads
+
+from models import *
 
 
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024)
         raw = data.decode('utf-8')
-        print(raw)
-        self.request.send('JADA DET ER FINT, DET!!'.encode('utf-8'))
+        try:
+            json = loads(raw)
+            request = json['request']
+            content = json['content']
+            self.server.chatserver.handle_command(request, content)
+        except ValueError:
+            self.request.send('400 JSON malformed.'.encode('utf-8'))
+
 
 class KTNServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     daemon_threads = True
@@ -18,21 +27,36 @@ class KTNServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
         socketserver.TCPServer.__init__(self, server_address, RequestHandlerClass)
 
 
-def print_usage():
-    print('Usage: python server.py <port>')
 
-def server():
-    port = int(argv[1])
-    host = '127.0.0.1'
+class ChatServer:
 
-    server = KTNServer((host, port), TCPHandler)
-    server.serve_forever()
+    def __init__(self, port, host):
+        server = KTNServer((host, port), TCPHandler)
+        server.chatserver = self
+        server.serve_forever()
+
+        self.messages = []
+        self.users = []
+
+    def handle_command(self, req, content):
+        print('req:', req)
+        print('content:', content)
+        if req == 'msg':
+            msg = Message(None, content)
+            mesasges.append(msg)
+            #broadcast
+        else:
+             pass
+
+
 
 # Hvis vi kjører filen med `python server.py`
 if __name__ == '__main__':
     if len(argv) < 2:
-        print_usage()
+        print('Usage: python server.py <port>')
     elif version_info < (3, 0): 
         print('Python 3.0 or higher required')
     else:
-        server()
+        port = int(argv[1])
+        host = '127.0.0.1'
+        ChatServer(port, host)
